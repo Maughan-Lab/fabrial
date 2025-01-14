cb0e45b654299e34c9b78935ce2a0a83ecde0a13
Drew <drewlwhitney@gmail.com>
2025-01-14 09:52:05 -0700

    Getting the rest from the last commit.

---
+++ src/utility/timers.py
@@ -0,0 +1,40 @@
+from PyQt6.QtCore import QTimer, QObject
+from typing import Callable, Any
+
+
+class Timer(QTimer):
+    """Easier QTimer class."""
+
+    def __init__(self, parent: QObject, intverval_ms: int, *slots: Callable[[], Any]):
+        """
+        Instantiate a new timer and automatically connect the passed in **slots**.
+
+        :param parent: The QObject that owns this timer.
+        :param interval_ms: The timeout interval in milliseconds.
+        :param slots: Function(s) to call when the timer times out.
+        """
+        super().__init__()
+        self.setInterval(intverval_ms)
+        for slot in slots:
+            self.timeout.connect(slot)
+
+    def start_fast(self):
+        """Start the timer and instantly emit the timeout signal."""
+        self.start()
+        self.timeout.emit()
+
+
+def new_timer(parent: QObject, interval_ms: int, *slots: Callable[[], Any]) -> QTimer:
+    """
+    Instantiate a new timer. **slots** are called immediately.
+
+    :param parent: The QObject that owns this timer.
+    :param interval_ms: The timeout in milliseconds.
+    :param slots: The function(s) to connect the timer's timeout signal to.
+
+    :returns: The created timer. The timer is already started. Note that you must keep a reference to the
+    timer so it does not get deleted.
+    """
+    timer = Timer(parent, interval_ms, *slots)
+    timer.start_fast()
+    return timer
